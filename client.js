92401e63894011b6ef19a24e51b4aaac9e1b2fbc
SHSupport24 <j.hantl@serverhero.de>
2025-05-13 14:49:04 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -1,26 +1,27 @@
-/* global TrelloPowerUp */
-const t = TrelloPowerUp.iframe();
-
-window.openTrackingPopup = function (t) {
-  return t.popup({
-    title: 'Sendungsverfolgung',
-    url: './tracking.html',
-    height: 300
-  });
-};
-
-window.getStatusBadge = function (t) {
-  return t.get('card', 'shared', 'trackingStatus')
-    .then(status => {
-      if (!status) return [];
-
-      let color = 'yellow';
-      if (status === 'Zugestellt') color = 'green';
-      if (status === 'Ausstehend') color = 'red';
-
-      return [{
-        text: status,
-        color: color
-      }];
-    });
-};
+window.TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      text: 'Sendung verfolgen',
+      callback: function(t) {
+        return t.popup({
+          title: 'Sendung verfolgen',
+          url: './tracking.html',
+          height: 250
+        });
+      }
+    }];
+  },
+  'card-badges': function(t, options) {
+    return t.get('card', 'shared', 'trackingStatus')
+      .then(function(status) {
+        if (!status) return [];
+        let color = 'orange';
+        if (status === 'Zugestellt') color = 'green';
+        if (status === 'Ausstehend') color = 'red';
+        return [{
+          text: status,
+          color: color
+        }];
+      });
+  }
+});
